1b744213e978cb0464a129d37137eaecf014abfd
George Mitenkov <georgemitenk0v@gmail.com>
2022-07-14 16:23:41 +0200

    [Aggregator] Aggregator u128-and-Table implementation

--- aptos-move/framework/tests/move_unit_test.rs
+++ aptos-move/framework/tests/move_unit_test.rs
@@ -2,16 +2,19 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use aptos_types::account_config::CORE_CODE_ADDRESS;
-use aptos_vm::move_vm_ext::test_transaction_context_natives;
+use aptos_vm::move_vm_ext::{aggregator_natives, NativeAggregatorContext, test_transaction_context_natives};
 use framework::path_in_crate;
 use move_deps::{
     move_cli::package::cli, move_stdlib, move_table_extension, move_unit_test::{UnitTestingConfig, extensions},
     move_vm_runtime::{native_functions::NativeFunctionTable, native_extensions::NativeContextExtensions},
+    move_vm_test_utils::BlankStorage,
 };
+use once_cell::sync::Lazy;
 use tempfile::tempdir;
 
 fn run_tests_for_pkg(path_to_pkg: impl Into<String>) {
     let pkg_path = path_in_crate(path_to_pkg);
+    extensions::set_extension_hook(Box::new(add_aggregator_context));
     cli::run_move_unit_tests(
         &pkg_path,
         move_deps::move_package::BuildConfig {
@@ -26,6 +29,13 @@ fn run_tests_for_pkg(path_to_pkg: impl Into<String>) {
     .unwrap();
 }
 
+static DUMMY_RESOLVER: Lazy<BlankStorage> = Lazy::new(|| BlankStorage);
+
+// Hook to pass dummy aggregator context to Move unit tests.
+fn add_aggregator_context(ext: &mut NativeContextExtensions) {
+    ext.add(NativeAggregatorContext::new(0, &*DUMMY_RESOLVER))
+}
+
 // move_stdlib has the testing feature enabled to include debug native functions
 pub fn aptos_test_natives() -> NativeFunctionTable {
     move_stdlib::natives::all_natives(CORE_CODE_ADDRESS)
@@ -33,6 +43,7 @@ pub fn aptos_test_natives() -> NativeFunctionTable {
         .chain(framework::natives::all_natives(CORE_CODE_ADDRESS))
         .chain(move_table_extension::table_natives(CORE_CODE_ADDRESS))
         .chain(test_transaction_context_natives(CORE_CODE_ADDRESS))
+        .chain(aggregator_natives(CORE_CODE_ADDRESS))
         .collect()
 }
 
